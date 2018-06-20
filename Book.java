c946b132a48c770b09a1d5b29b8a68d53be21ade
moonfull101 <40380980+moonfull101@users.noreply.github.com>
2018-06-20 20:51:39 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -2,45 +2,62 @@ package author;
 
 public class Book extends Author {
 	
-	private String name; 
+	private String nameOfABook; 
 	private Author author; 
 	private double price;
 	private int quantity; 
+	private String NameAuthor; 
 	
 	public Book (String name, Author author, double price) {
-		this.name = name; 
+		this.nameOfABook = name; 
 		this.author = author; 
 		this.price = price; 
 	}
 	
 	public Book (String name, Author author, double price, int quantity) {
-		this.name = name; 
+		this.nameOfABook = name; 
 		this.author = author; 
 		this.price = price; 
 		this.quantity = quantity; 
 	}
-	public String getName () {
-		return name; 
+	public String getNameOfABook (String nameOfABook) {
+		return nameOfABook; 
 	}
-		public Author getAuthor() {
+		public Author getAuthor(Author author) {
 			return author; 
+		
 		}
-		public double getPrice () {
+		public double getPrice (double price) {
 			return price; 
 		}
 		public void setPrice (double newPrice) {
 			this.price = newPrice; 
 		}
-		public int getQty () {
+		public int getQty (int quantity) {
 			return quantity; 
 		}
 		public void setQty (int newQuantity) {
 			this.quantity = newQuantity;
 		}
 		public String toString () {
-			return "Book[name = " + name + "Author[name= " + name + "email = " + email + "gender + " + gender +"], price = "+ price + "quantity = "+ quantity; 
+			return "Book[name = " + nameOfABook + " Author[name= " + nameOfABook + " email = " + email + " gender + " + gender +"], price = "+ price + " quantity = "+ quantity; 
 		}
-}
+
+		public String getAuthor(String name) {
+			return name;
+		}
+
+		public void setAuthorName(String nameAuthor) {
+			// TODO Auto-generated method stub
+			this.NameAuthor = nameAuthor;
+		}
+		
+			public String getAuthorName (String nameAuthornow) {
+				return nameAuthornow; 
+			}
+		}
+
+
 
 
 
